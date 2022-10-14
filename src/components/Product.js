6ee7c495ecffee18c93367c261334b64d28f1767
Burak Furkan Tenekeci <furkan_te@hotmail.com>
2022-10-14 11:22:43 +0300

    favicon

--- src/components/Product.js
+++ src/components/Product.js
@@ -28,7 +28,7 @@ const Product = ({image,price,type,stars,title,isvisible}) => {
     <AnimatePresence>
       {isvisible && (
         <motion.div
-          initial={{ opacity: 0, y: 60 }}
+          initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 60 }}
           transition={{ duration: 0.5 }}
